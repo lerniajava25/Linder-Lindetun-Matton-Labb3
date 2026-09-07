f25c1d2036b076f73dfc9cabb26072ae85806768
William Matton <williammatton@gmail.com>
2026-09-07 13:22:36 +0200

    Initial commit.

---
+++ src/main/java/org/example/linderlindetunmattonlabb3/LinderLindetunMattonLabb3Application.java
@@ -0,0 +1,13 @@
+package org.example.linderlindetunmattonlabb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinderLindetunMattonLabb3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinderLindetunMattonLabb3Application.class, args);
+    }
+
+}
