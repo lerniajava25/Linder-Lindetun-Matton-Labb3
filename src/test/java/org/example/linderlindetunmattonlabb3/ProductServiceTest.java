e9161aba8cfc97452221ab9f70f8ee92b63e3c77
leolinder-hash <leolinder074@gmail.com>
2026-09-09 17:34:02 +0200

    add JUnit tests for price sorting logic

---
+++ src/test/java/org/example/linderlindetunmattonlabb3/ProductServiceTest.java
@@ -0,0 +1,103 @@
+package org.example.linderlindetunmattonlabb3;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    private ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService();
+
+        Product laptop = new Product(
+                "1",
+                "Laptop",
+                12000.0,
+                "Electronics",
+                10,
+                LocalDate.of(2027, 12, 31),
+                LocalDate.now()
+        );
+
+        Product mouse = new Product(
+                "2",
+                "Mouse",
+                500.0,
+                "Electronics",
+                25,
+                LocalDate.of(2028, 1, 31),
+                LocalDate.now()
+        );
+
+        Product monitor = new Product(
+                "3",
+                "Monitor",
+                4000.0,
+                "Electronics",
+                8,
+                LocalDate.of(2028,6,15),
+                LocalDate.now()
+        );
+
+        Product keyboard = new Product(
+                "4",
+                "Keyboard",
+                1000.0,
+                "Electronics",
+                15,
+                LocalDate.of(2028,7,17),
+                LocalDate.now()
+        );
+
+        productService.addProduct(laptop);
+        productService.addProduct(mouse);
+        productService.addProduct(monitor);
+        productService.addProduct(keyboard);
+    }
+
+    @Test
+    void shouldReturnMostExpensiveProductsInDescendingOrder(){
+        List<Product> result = productService.getMostExpensiveProducts(3);
+
+        assertEquals(3, result.size());
+        assertEquals("Laptop", result.get(0).getName());
+        assertEquals("Monitor", result.get(1).getName());
+        assertEquals("Keyboard", result.get(2).getName());
+    }
+
+    @Test
+    void shouldReturnEmptyListIfLimitEqualsZero(){
+        List<Product> result = productService.getMostExpensiveProducts(0);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentExceptionIfLimitIsLessThanZero(){
+        assertThrows(
+            IllegalArgumentException.class,
+                ()-> productService.getMostExpensiveProducts(-3)
+        );
+    }
+
+    @Test
+    void shouldReturnAllProductsIfLimitIsGreaterThanNumberOfProducts(){
+        List<Product> result = productService.getMostExpensiveProducts(12);
+        assertEquals(4, result.size());
+    }
+
+    @Test
+    void shouldReturnEmptyListIfNoProductsExists(){
+        ProductService emptyProductService = new ProductService();
+
+        List<Product> result = emptyProductService.getMostExpensiveProducts(5);
+
+        assertTrue(result.isEmpty());
+    }
+}
