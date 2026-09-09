bcdedf64eafcee6849ea97fbdee3549a32ea66cd
tavla75 <joseph.lindetun@gmail.com>
2026-09-09 12:33:02 +0200

    .

--- src/main/java/org/example/linderlindetunmattonlabb3/Product.java
+++ src/main/java/org/example/linderlindetunmattonlabb3/Product.java
@@ -1,15 +1,26 @@
 package org.example.linderlindetunmattonlabb3;
 
+import java.time.LocalDate;
+
 
 public class Product {
     private String id;
     private String name;
     private double price;
+    private String category;
+    private int stockBalance;
+    private LocalDate expirationDate;
+    private LocalDate registrationDate;
 
-    public Product(String id, String name, double price){
+
+    public Product(String id, String name, double price, String category, int stockBalance, LocalDate expirationDate, LocalDate registrationDate) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.category = category;
+        this.stockBalance = stockBalance;
+        this.expirationDate = expirationDate;
+        this.registrationDate = registrationDate;
     }
     public String getId(){
         return id;
@@ -19,5 +30,17 @@ public class Product {
     }
     public double getPrice(){
         return price;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public int getStockBalance(){
+        return stockBalance;
+    }
+    public LocalDate getExpirationDate(){
+        return expirationDate;
+    }
+    public LocalDate getRegistrationDate(){
+        return registrationDate;
     }
 }
